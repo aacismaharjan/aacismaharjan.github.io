1887b86ab52a1a9807a9ef577c62bffee97cf50d
= <=>
2023-03-20 14:40:46 +0545

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.abc2823e86144a2d71d0e1374ac75915.js"
+  "/precache-manifest.5feee488bb853358c1bc8a7f71f8c8fc.js"
 );
 
 self.addEventListener('message', (event) => {
